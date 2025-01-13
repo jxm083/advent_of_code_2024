b82af1ec13b040c1419d98d92282b6eb80493b65
Joseph D. Murphree <joseph.murphree@protonmail.com>
2025-01-13 17:44:46 -0600

    Added notes about how to activate the environment and removed hello world programs.

--- hello_world/target/debug/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-C:\Users\josep\programming\projects\advent_of_code_2024\hello_world\target\debug\deps\hello_world.exe: src\main.rs
-
-C:\Users\josep\programming\projects\advent_of_code_2024\hello_world\target\debug\deps\hello_world.d: src\main.rs
-
-src\main.rs:
