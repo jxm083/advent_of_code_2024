b25f0ee1aecf4bb1678159bf7bc8fc43af441ead
Joseph D. Murphree <joseph.murphree@protonmail.com>
2025-01-14 19:13:51 -0600

    Added the number data as separate files.

--- day01/distance_finder.py
+++ day01/distance_finder.py
@@ -1,3 +1,23 @@
+import os
+print(os.getcwd())
+
+def import_nums(file_name):
+    """
+    Takes in a file_name and returns two arrays of ints.
+    """
+    file = open(file_name, "r")
+    numbers_all = []
+
+    for line in file.readlines():
+        line.strip() # remove leading and trailing spaces
+        nums = line.split(" ")
+        nums_clean = [int(num.strip()) for num in nums if num != '']
+        print(nums_clean)
+        numbers_all.append(nums_clean)
+
+    return numbers_all
+
+
 list_a = [3, 4, 2, 1, 3, 3]
 list_b = [4, 3, 5, 3, 9, 3]
 
@@ -9,4 +29,6 @@ def sorted_list_difference(a, b):
 
     return sum(dif_ab)
 
+print()
+print(import_nums("numbers00.csv"))
 print(sorted_list_difference(list_a, list_b))
