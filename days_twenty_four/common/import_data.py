f2646d89935e59e4edb8e3e0880e280ecb4ba06e
Joseph D. Murphree <joseph.murphree@protonmail.com>
2025-01-22 11:02:33 -0600

    Updated file specification in import_data

--- days_twenty_four/common/import_data.py
+++ days_twenty_four/common/import_data.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 
-def import_data(file_name: str, file_path: Path = Path(os.getcwd())) -> list:
+def import_data(file_name: str, file_dir: Path = Path(os.getcwd())) -> list:
     """
     Takes in a file_name and returns two arrays of ints.
     Must be run in the same directory as the data.
@@ -11,20 +11,22 @@ def import_data(file_name: str, file_path: Path = Path(os.getcwd())) -> list:
 
     Args:
         file_name (str): name of file containing list of location ints
-        file_path (Path): a path object specifying the directory containing the file; defaults to current working directory
+        file_dir (Path): a path object specifying the directory containing the file; defaults to current working directory
 
     Returns:
         list: A list of the location int lists
     """
-    print(file_path)
-    file = open(file_name, "r")
+    print(file_dir)
+    file_path = file_dir / Path(file_name)
     numbers_all = []
 
-    for line in file.readlines():
-        line.strip() # remove leading and trailing spaces
-        nums = line.split(" ")
-        nums_clean = [int(num.strip()) for num in nums if num != '']
-        numbers_all.append(nums_clean)
+    with file_path.open() as file:
+
+        for line in file.readlines():
+            line.strip() # remove leading and trailing spaces
+            nums = line.split(" ")
+            nums_clean = [int(num.strip()) for num in nums if num != '']
+            numbers_all.append(nums_clean)
         
     # Reshape so that each element of data is a column of the original file
     data = [[nums[i] for nums in numbers_all] for i in range(len(numbers_all[0]))]
