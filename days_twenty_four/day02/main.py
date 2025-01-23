7818d555cfc164df7e27200b4ffbc8447f090ba8
Joseph D. Murphree <joseph.murphree@protonmail.com>
2025-01-22 21:49:38 -0600

    got exercise two wrong; added troubleshooting print statement

--- days_twenty_four/day02/main.py
+++ days_twenty_four/day02/main.py
@@ -4,16 +4,17 @@ from days_twenty_four.common.import_data import import_data
 
 DATA_DIR = Path(__file__).parents[0]
 
-def is_safe(level_list: list[int]):
+def is_safe(level_list: list[int], dampner: bool = False) -> bool:
     """
     Takes in a list of levels and deems it safe if
     (a) the levels are all increasing or all decreasing, and
     (b) any two adjacent levels differ by at least one and at most three
 
-    Assumes level_list has at least two elements.
+    level_list must be at least two elements.
 
     Args:
-        level_list (list[int]) the list of levels
+        level_list (list[int]): the list of levels
+        dampner (bool): indicates if the dampner is engaged
 
     Returns:
         bool: whether the levels are safe or not
@@ -28,6 +29,7 @@ def is_safe(level_list: list[int]):
     previous_diff = level_list[0] - level_list[1]
 
     safe = True
+    bad_level_cnt = 0
 
     # Starting with the second level, find the differences
     # between adjacent levels
@@ -41,6 +43,11 @@ def is_safe(level_list: list[int]):
             previous_level = level
             previous_diff = diff
 
+        # If it is the first bad level with the dampner on, note it and continue
+        elif bad_level_cnt == 0 and dampner:
+            bad_level_cnt += 1
+            print(f"Unsafe level: {level}")
+
         else:
             safe = False
             break
@@ -48,8 +55,8 @@ def is_safe(level_list: list[int]):
     return safe
 
 
-def exercise_one():
-    data = import_data("data01.csv", DATA_DIR)
+def exercise_one(file_name: str | None = "data01.csv", file_dir: Path | None = DATA_DIR):
+    data = import_data(file_name, file_dir)
 
     safe_levels = 0
 
@@ -59,5 +66,17 @@ def exercise_one():
     
     return safe_levels
 
+def exercise_two(file_name: str | None = "data01.csv", file_dir: Path | None = DATA_DIR):
+    data = import_data(file_name, file_dir)
+
+    safe_levels = 0
+
+    for levels in data:
+        if is_safe(levels, dampner = True):
+            safe_levels += 1
+
+    return safe_levels
+
 if __name__ == "__main__":
-    print(f"Number of safe levels: {exercise_one()}")
+    print(f"Number of safe levels: {exercise_one()}") # 341
+    print(f"Number of safe levels w/ dampner: {exercise_two("data00.csv")}") # 378
