13f2da07a75265b96cf0d9eff30f3719a77bdde0
Joseph D. Murphree <joseph.murphree@protonmail.com>
2025-01-27 16:06:56 -0600

    created line_to_funcs

--- advent/advent_of_code_2024/day03/main.py
+++ advent/advent_of_code_2024/day03/main.py
@@ -1,2 +1,20 @@
+def line_to_funcs(line: str, func_names: list[str] | None = ["mul"]) -> list[str]:
+    """
+    Takes a line of corrupted code and returns
+    a list of correct instructions. Defaults to the "mul" function.
+    
+    Assumes functions will not be nested.
+
+    Args:
+        line (str): a string of corrupt code
+        func_names (list[str]): a list of valid function names, defaults to ["mul"]
+
+    Returns:
+        list[str]: a list containing the strings of all valid functions
+    """
+    funcs: list[str] = []
+
+    return funcs
+
 def exercise_one():
     pass
--- tests/advent_of_code_2024/day03/test_main03.py
+++ tests/advent_of_code_2024/day03/test_main03.py
@@ -1,11 +1,37 @@
 import pytest
 
-from advent.advent_of_code_2024.day03.main import exercise_one
+from advent.advent_of_code_2024.day03.main import (
+    exercise_one,
+    line_to_funcs
+) 
+
+from pathlib import Path
 
 @pytest.fixture
 def test_line() -> str:
-    test_line = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-    return test_line
+    line = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    return line
+
+@pytest.fixture
+def test_data_file(
+        test_line: str,
+        tmp_path: Path
+) -> Path:
+    line: str = test_line
+
+    fake_path = tmp_path / "fake.txt"
+    fake_path.write_text(line)
+
+    return fake_path
+
+def test_line_to_funcs(test_line: str):
+    answer: list[str] = [
+        "mul(2,4)",
+        "mul(5,5)",
+        "mul(11,8)",
+        "mul(8,5)"
+    ]
+    assert line_to_funcs(test_line) == answer
 
 def test_exercise_one():
-    exercise_one()
+    assert exercise_one() == 161
