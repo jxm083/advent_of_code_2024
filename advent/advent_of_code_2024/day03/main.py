03bc3e62d297310851adb9d287fac18a42307af6
Joseph D. Murphree <joseph.murphree@protonmail.com>
2025-01-28 16:28:34 -0600

    got rough pattern matching to work

--- advent/advent_of_code_2024/day03/main.py
+++ advent/advent_of_code_2024/day03/main.py
@@ -16,12 +16,8 @@ def line_to_funcs(line: str, func_names: list[str] | None = ["mul"]) -> list[str
     """
     funcs: list[str] = []
 
-    # All function calls will start with "func(", so append "(" to the func names
-    # note that the open parenthesis needs to be escaped to be used by re
-    func_starts: list[str] = [name + "\(" for name in func_names] # type: ignore
-    print(func_starts)
-
-    print([m.start() for m in re.finditer(func_starts[0], line)]) # type: ignore
+    pattern = re.compile('mul\([\d]{1,3},[\d]{1,3}\)')
+    funcs = pattern.findall(line)
 
     return funcs
 
@@ -32,4 +28,4 @@ TEST_LINE = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8
 
 if __name__ == "__main__":
     print("test:")
-    line_to_funcs(TEST_LINE)
+    print(line_to_funcs(TEST_LINE))
