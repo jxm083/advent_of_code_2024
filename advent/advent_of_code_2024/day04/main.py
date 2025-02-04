cb1f8c1bbfe543e6b6c8523c9d864036ea4b5ee9
Joseph D. Murphree <joseph.murphree@protonmail.com>
2025-02-03 21:53:46 -0600

    added generate_text_block function

--- advent/advent_of_code_2024/day04/main.py
+++ advent/advent_of_code_2024/day04/main.py
@@ -1,8 +1,39 @@
 from pathlib import Path
 
-DATA_DIR = Path(__file__)
+DATA_DIR = Path(__file__).parent
+DATA_TEST = DATA_DIR / "data00.txt"
+DATA_P1 = DATA_DIR / "data01.txt"
 
 TARGET_STR = "XMAS"
 
-def exercise_one(file_path: Path = DATA_DIR / "data01.txt") -> int:
+def text_line(
+        data: list[str], 
+        position: tuple[int, int],
+        direction: tuple[int, int], 
+        length: int
+    ) -> str:
+
     pass
+
+def generate_text_block(file_path: Path, lin_count: int) -> list[str]:
+    data: list[str] = []
+    count = 0
+    with file_path.open() as file:
+        while count < lin_count:
+            data.append(file.readline())
+            count += 1
+
+    return data
+    
+
+
+
+
+def exercise_one(file_path: Path = DATA_P1) -> int:
+    with file_path.open() as file:
+        for line in file.readlines():
+            print(line)
+
+if __name__ == "__main__":
+    print(generate_text_block(DATA_TEST, 2))
+    exercise_one(DATA_TEST)
--- tests/advent_of_code_2024/day04/test_main04.py
+++ tests/advent_of_code_2024/day04/test_main04.py
@@ -3,12 +3,20 @@
 from pathlib import Path
 
 from advent.advent_of_code_2024.day04.main import (
+    generate_text_block,
     exercise_one
 )
 
 PACKAGE_ROOT_LEVEL = 3
 ROOT_PACKAGE_DIR = Path(__file__).parents[PACKAGE_ROOT_LEVEL]
-DATA_DIR = ROOT_PACKAGE_DIR / "advent_of_code_2024" / "day04"
+DATA_DIR = ROOT_PACKAGE_DIR / "advent" / "advent_of_code_2024" / "day04"
+DATA_TEST = DATA_DIR / "data00.txt"
+
+def test_generate_text_block():
+    assert generate_text_block(DATA_TEST, 2) == ['MMMSXXMASM\n', 'MSAMXMSMSA\n']
 
 def test_exercise_one_example():
     assert exercise_one(DATA_DIR / "data00.txt") == 18
+
+if __name__ == "__main__":
+    print(DATA_TEST.exists)
