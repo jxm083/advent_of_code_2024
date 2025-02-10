8e451fd742b63944f5b8d45c47bef8743e774813
Joseph D. Murphree <joseph.murphree@protonmail.com>
2025-02-10 17:40:17 -0600

    added test functions for importing exercise one data

--- advent/advent_of_code_2024/day05/main.py
+++ advent/advent_of_code_2024/day05/main.py
@@ -1,2 +1,17 @@
+from pathlib import Path
+
+DATA_DIR: Path = Path(__file__).parent
+DATA_EXAMPLE_01: Path = DATA_DIR / "data_example_1.txt"
+DATA_01: Path = DATA_DIR / "data_01.txt"
+
+def import_rules(file_path: Path = DATA_01) -> dict[int, list[int]]:
+    pass
+
+def import_draft_page_lists(file_path: Path = DATA_01) -> list[list[int]]:
+    pass
+
+def exercise_one(file_path: Path = DATA_01) -> int:
+    pass
+
 if __name__ == "__main__":
     pass
--- tests/advent_of_code_2024/day05/test_main05.py
+++ tests/advent_of_code_2024/day05/test_main05.py
@@ -1,2 +1,45 @@
+import pytest
+
+from pathlib import Path
+
 from advent.advent_of_code_2024.day05.main import (
+    exercise_one,
+    import_rules,
+    import_draft_page_lists
 )
+
+PACKAGE_ROOT_LEVEL: int = 3 # TODO: make this better than a relative path
+ROOT_PACKAGE_DIR: Path = Path(__file__).parents[PACKAGE_ROOT_LEVEL]
+DATA_DIR = ROOT_PACKAGE_DIR / "advent" / "advent_of_code_2024" / "day05"
+DATA_EXAMPLE_ONE = DATA_DIR / "data_example_1.txt"
+
+@pytest.fixture
+def example_one_rules() -> dict[int, list[int]]:
+    return {
+        47: [53, 13, 61, 29, 61],
+        97: [13, 61, 47, 29, 53, 75],
+        75: [29, 47, 13, 61],
+        61: [13, 53, 29],
+        29: [13],
+        53: [29, 13]
+    }
+
+@pytest.fixture
+def example_one_draft_page_lists() -> list[list[int]]:
+    return [
+        [75, 47, 61, 53, 29],
+        [97, 61, 53, 29, 13],
+        [75, 29, 13],
+        [75, 97, 47, 61, 53],
+        [61, 13, 29],
+        [97, 13, 75, 29, 47]
+    ]
+
+def test_import_rules(example_one_rules: dict[int, list[int]]):
+    assert import_rules(DATA_EXAMPLE_ONE) == example_one_rules
+
+def test_import_draft_page_lists(example_one_draft_page_lists: list[list[int]]):
+    assert import_draft_page_lists(DATA_EXAMPLE_ONE) == example_one_draft_page_lists
+
+def test_exercise_one_example():
+    assert exercise_one(DATA_EXAMPLE_ONE) == 143
