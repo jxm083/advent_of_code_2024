955db13555a873c00cbbfe81e007114f6df3fa97
Joseph D. Murphree <joseph.murphree@protonmail.com>
2025-02-15 16:20:06 -0600

    modified stream_lines_from_file to prevent it from stopping after blank line

--- advent/common/data_stream.py
+++ advent/common/data_stream.py
@@ -9,8 +9,8 @@ U = TypeVar("U")
 
 def stream_lines_from_file(f_path: Path) -> Generator[str, None, None]:
     with f_path.open() as f:
-        while line := f.readline().rstrip():
-            yield line
+        while line := f.readline():
+            yield line.rstrip()
 
 def parse_line_to_ints(line: str) -> Generator[int, None, None]:
     for i in line.split():
