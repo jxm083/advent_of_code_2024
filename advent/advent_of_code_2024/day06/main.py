895ec9ea013e927896968e69d23cc8ec1083b1ce
Joseph D. Murphree <joseph.murphree@protonmail.com>
2025-02-21 17:04:11 -0600

    added exercise_one definition

--- advent/advent_of_code_2024/day06/main.py
+++ advent/advent_of_code_2024/day06/main.py
@@ -7,8 +7,8 @@ DATA_DIR: Path = Path(__file__).parent
 DATA_00_PATH: Path = DATA_DIR / "data_00_example_1.txt"
 DATA_01_PATH: Path = DATA_DIR / "data_01.txt"
 
-def parse_guard_direction(arrow: str) -> tuple[int, int]:
-    direction: tuple[int, int] = (0, 0)
+def parse_guard_direction(arrow: str) -> tuple[int, int] | None:
+    direction: tuple[int, int] | None = (0, 0)
 
     match arrow:
         case ">":
@@ -20,7 +20,7 @@ def parse_guard_direction(arrow: str) -> tuple[int, int]:
         case "^":
             direction = (0, -1)
         case _:
-            raise ValueError
+            direction = None
     
     return direction
 
@@ -70,15 +70,46 @@ def calc_next_step(location: tuple[int, int], direction: tuple[int, int], map_di
 
     return loc
 
-def exercise_one(file_path: Path = DATA_01_PATH):
+def calc_guard_path(location: tuple[int, int], direction: tuple[int, int], map_dict: dict[tuple[int, int], str]) -> list[tuple[int, int]]:
+    guard_path: list[tuple[int, int]] = [location]
+    next_location: tuple[int, int] | None = calc_next_step(location, direction, map_dict)
+
+    while next_location is not None:
+        current_location = next_location
+        new_direction: tuple[int, int] = (
+            current_location[0] - guard_path[-1][0],
+            current_location[1] - guard_path[-1][1]
+        )
+        guard_path.append(current_location)
+        next_location = calc_next_step(current_location, new_direction, map_dict)
+
+    return guard_path
+
+def exercise_one(file_path: Path = DATA_01_PATH) -> int:
     line_stream = stream_lines_from_file(file_path)
     map_stream = parse_lines_to_grid_entries(line_stream)
     map_dict: dict[tuple[int, int], str] = {}
+    guard_path: list[tuple[int, int]] = []
+    guard_pos_init: tuple[int, int] | None = None
+    guard_direction_init: tuple[int, int] | None = None
 
     for xcor, ycor, character in map_stream:
-        map_dict[(xcor, ycor)] = character
+        pos = (xcor, ycor)
+        map_dict[pos] = character
 
-    print(map_dict)
+        if len(guard_path) == 0:
+            guard_direction_init = parse_guard_direction(character)
+            if guard_direction_init is not None:
+                guard_pos_init = pos
+
+    if guard_direction_init is not None and guard_pos_init is not None:
+        guard_path = calc_guard_path(
+            location=guard_pos_init,
+            direction=guard_direction_init,
+            map_dict=map_dict
+        )
+    
+    return len(guard_path)
 
 if __name__ == "__main__":
     print(DATA_00_PATH)
