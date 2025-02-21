dc94773750bfc223a35878e2bdbb64e49b8f6948
Joseph D. Murphree <joseph.murphree@protonmail.com>
2025-02-21 15:05:39 -0600

    added update_direction function and calc_next_step

--- advent/advent_of_code_2024/day06/main.py
+++ advent/advent_of_code_2024/day06/main.py
@@ -10,16 +10,41 @@ def parse_lines_to_grid_entries(file_stream: Generator[str, None, None]) -> Iter
         for char_num, character in enumerate(line):
             yield (line_num, char_num, character)
 
+def update_direction(direction: tuple[int, int], reverse: bool = False) -> tuple[int, int]:
+    """
+    returns the updated direction when the guard reaches an obstacle.
+    
+    assumes the guard turns right, by default
+    """
+    directions: list[tuple[int, int]] = [
+        (0, 1),
+        (1, 0),
+        (0, -1),
+        (-1, 0)
+    ]
+    ind_change: int = 1
+
+    if reverse:
+        ind_change = -1
+
+    new_direction: tuple[int, int] = directions[(directions.index(direction) + ind_change) % 4]
+
+    return new_direction
+
 def calc_next_step(location: tuple[int, int], direction: tuple[int, int], map_dict: dict[tuple[int, int], str]) -> tuple[int, int] | None:
-    loc_x: int = location[0] + direction[0]
-    loc_y: int = location[0] + direction[0]
+    loc: tuple[int, int] | None = (
+        location[0] + direction[0],
+        location[1] + direction[1]
+    )
 
-    loc: tuple[int, int] = (loc_x, loc_y)
-
-    if loc in map_dict.keys:
-        pass
+    if loc in map_dict:
+        if map_dict[loc] == "#":
+            new_direction: tuple[int, int] = update_direction(direction)
+            loc = calc_next_step(location, new_direction, map_dict)
     else:
-        return None
+        loc = None
+
+    return loc
 
 def exercise_one(file_path: Path = DATA_01_PATH):
     pass
--- tests/advent_of_code_2024/day06/test_main06.py
+++ tests/advent_of_code_2024/day06/test_main06.py
@@ -2,11 +2,19 @@ from pathlib import Path
 
 from advent.common.data_stream import stream_lines_from_file
 from advent.advent_of_code_2024.day06.main import (
-    parse_lines_to_grid_entries
+    parse_lines_to_grid_entries,
+    update_direction
 )
 import advent.advent_of_code_2024.day06.main as test_script
 DATA_DIR: Path = Path(test_script.__file__).parent
 DATA_PATH_00 = DATA_DIR / "data_00_example_1.txt"
+
+def test_update_direction():
+    assert update_direction((0, 1)) == (1, 0)
+    assert update_direction((0, 1), reverse=True) == (-1, 0)
+    assert update_direction((1, 0)) == (0, -1)
+    assert update_direction((0, -1)) == (-1, 0)
+    assert update_direction((-1, 0)) == (0, 1)
 
 first_test_entries: list[tuple[int, int, str]] = [
     (0, 0, "."),
