93cc6d2ace91274cf4cf904bacfa87d65720ebc7
Joseph D. Murphree <joseph.murphree@protonmail.com>
2025-03-07 17:05:40 -0600

    takewhile_pair handling the empty list edge case

--- advent/common/extended_itertools.py
+++ advent/common/extended_itertools.py
@@ -4,6 +4,7 @@ from collections import deque
 
 T = TypeVar("T")
 
+
 def sliding_window(iterable: Iterable[T], n: int) -> Iterator[tuple[T, ...]]:
     "Collect data into overlapping fixed-length chunks or blocks."
     # sliding_window('ABCDEFG', 4) → ABCD BCDE CDEF DEFG
@@ -14,9 +15,25 @@ def sliding_window(iterable: Iterable[T], n: int) -> Iterator[tuple[T, ...]]:
         yield tuple(window)
 
 
-def takewhile_pair(predicate: Callable[[T], bool], iterable: Iterable[T]) -> Iterator[T]:
+def takewhile_pair(
+    predicate: Callable[[T], bool], iterable: Iterable[T]
+) -> Iterator[T]:
+    """
+    Produces an iterator that does not stop until either two
+    sequential items evaluate to false when entered into
+    predicate (and these items are excluded)
+    or the end of the original iterator is reached.
+
+    e.g. [0, -1, 1, -2, 2, -3, -4] -> [0, -1, 1, -2, 2]
+    """
     iterator = iter(iterable)
-    last_item: T = next(iterator)
+    last_item: T | None = None
+
+    if last_item is None:
+        try:
+            last_item = next(iterator)
+        except StopIteration:
+            return
 
     for item in iterator:
         if not (predicate(item) or predicate(last_item)):
