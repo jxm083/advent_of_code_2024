faab1287d7cc97d42ca1f0b56ffa90aa8d7b0b3d
Joseph D. Murphree <joseph.murphree@protonmail.com>
2025-03-09 17:35:03 -0500

    defined functions outside the class

--- advent/common/vectors.py
+++ advent/common/vectors.py
@@ -4,8 +4,19 @@ from dataclasses import dataclass
 T = TypeVar('T')
 
 @dataclass(frozen=True)
-class Vector(list[T]):
-    def length(self) -> T:
-        total_length = self[0]
-        for product in (n * n for n in self):
+class Vector(list[int]):
+    components: list[int]
+    def length(self) -> int:
+        return sum(n * n for n in self.components)
+
+def vec_add(vec0: Vector, vec1: Vector) -> Vector:
+    return Vector([x + y for x, y in zip(vec0.components, vec1.components)])
+
+def vec_mul(fac: int, vec: Vector) -> Vector:
+    return Vector([fac * x for x in vec.components])
+
+def vec_dot(vec0: Vector, vec1: Vector) -> int:
+    return sum(x * y for x, y in zip(vec0.components, vec1.components))
+
+
 
--- tests/common/test_vectors.py
+++ tests/common/test_vectors.py
@@ -1,5 +1,42 @@
-from advent.common.vectors import Vector
+import pytest
+from advent.common.vectors import (
+    Vector,
+    vec_add,
+    vec_mul,
+    vec_dot
+)
 
-def test_length():
-    vec = Vector([1, 2, 3])
-    assert vec.length() == 14
+@pytest.fixture
+def vec0():
+    return Vector([1, 2, 3])
+
+@pytest.fixture
+def vec1():
+    return Vector([-1, -2, -3])
+
+def test_length(vec0: Vector):
+    assert vec0.length() == 14
+
+def test_vector_not_equal(vec0: Vector, vec1: Vector):
+    assert vec0.components != vec1.components
+
+def test_vector_addition(vec0: Vector, vec1: Vector):
+    assert vec_add(vec0, vec1).components == Vector([0, 0, 0]).components
+
+def test_scalar_multiplication():
+    fac: int = 3
+    vec: Vector = Vector([1, 4])
+
+    assert vec_mul(fac, vec).components == Vector([3, 12]).components
+
+def test_dot_product(vec0: Vector, vec1: Vector):
+    assert vec_dot(vec0, vec1) == -14
+
+
+if __name__ == "__main__":
+    vec_0 = Vector([1, 2, 3])
+    vec_1 = Vector([-1, -2 , -3])
+
+    print(1 + 3)
+    print(vec_0 + vec_1)
+    print(vec_0)
