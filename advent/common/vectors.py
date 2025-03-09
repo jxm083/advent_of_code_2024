a17d43df200dc1c16c5b326197e65c78b3f18268
Joseph D. Murphree <joseph.murphree@protonmail.com>
2025-03-09 18:13:56 -0500

    implemented basic Vector methods

--- advent/common/vectors.py
+++ advent/common/vectors.py
@@ -1,22 +1,52 @@
 from typing import TypeVar
 from dataclasses import dataclass
+from collections.abc import Sequence
 
 T = TypeVar('T')
 
 @dataclass(frozen=True)
-class Vector(list[int]):
+class Vector(Sequence): # type: ignore
     components: list[int]
-    def length(self) -> int:
+    def __len__(self):
+        return len(self.components)
+
+    def __getitem__(self, key: slice): # type: ignore
+        return self.components[key]
+
+    def __add__(self, other): # type: ignore
+        return Vector([x + y for x, y in zip(self.components, other.components)]) # type:ignore
+
+    def __sub__(self, other): # type: ignore
+        return Vector([x - y for x, y in zip(self.components, other.components)]) # type: ignore
+
+    def __mul__(self, other): # type: ignore
+        if type(other) is int: # type: ignore
+            return Vector([other * x for x in self.components])
+        else:
+            return Vector([x * y for x, y in zip(self.components, other.components)]) # type: ignore
+
+    def __rmul__(self, other): # type: ignore
+        if type(other) is int: # type: ignore
+            return Vector([other * x for x in self.components])
+        else:
+            return Vector([x * y for x, y in zip(self.components, other.components)]) # type: ignore
+
+    def __eq__(self, other): # type: ignore
+        equal = True
+        for x, y in zip(self.components, other.components): # type: ignore
+            if x != y:
+                equal = False
+
+        return equal
+
+    def __neg__(self):
+        return Vector([-x for x in self.components])
+
+    def dot(self, other: Sequence[int]) -> int:
+        return sum(x * y for x, y in zip(self.components, other.components)) # type: ignore
+
+    def magnitude(self) -> int:
         return sum(n * n for n in self.components)
-
-def vec_add(vec0: Vector, vec1: Vector) -> Vector:
-    return Vector([x + y for x, y in zip(vec0.components, vec1.components)])
-
-def vec_mul(fac: int, vec: Vector) -> Vector:
-    return Vector([fac * x for x in vec.components])
-
-def vec_dot(vec0: Vector, vec1: Vector) -> int:
-    return sum(x * y for x, y in zip(vec0.components, vec1.components))
 
 
 
--- tests/common/test_vectors.py
+++ tests/common/test_vectors.py
@@ -1,9 +1,6 @@
 import pytest
 from advent.common.vectors import (
     Vector,
-    vec_add,
-    vec_mul,
-    vec_dot
 )
 
 @pytest.fixture
@@ -14,28 +11,46 @@ def vec0():
 def vec1():
     return Vector([-1, -2, -3])
 
-def test_length(vec0: Vector):
-    assert vec0.length() == 14
+def test_magnitude(vec0: Vector):
+    assert vec0.magnitude() == 14
+
+def test_vector_get_item(vec0: Vector):
+    assert vec0[0] == 1
+    assert vec0[1] == 2
+    assert vec0[2] == 3
 
 def test_vector_not_equal(vec0: Vector, vec1: Vector):
-    assert vec0.components != vec1.components
+    assert vec0 != vec1
+
+def test_vector_equal(vec0: Vector):
+    assert vec0 == Vector([1, 2, 3])
 
 def test_vector_addition(vec0: Vector, vec1: Vector):
-    assert vec_add(vec0, vec1).components == Vector([0, 0, 0]).components
+    assert vec0 + vec1 == Vector([0, 0, 0])
+
+def test_vector_subtraction(vec0: Vector, vec1: Vector):
+    assert vec0 - vec1 == Vector([2, 4, 6])
 
 def test_scalar_multiplication():
     fac: int = 3
     vec: Vector = Vector([1, 4])
 
-    assert vec_mul(fac, vec).components == Vector([3, 12]).components
+    assert vec * fac == Vector([3, 12])
+
+    assert fac * vec == Vector([3, 12])
 
 def test_dot_product(vec0: Vector, vec1: Vector):
-    assert vec_dot(vec0, vec1) == -14
+    assert vec0.dot(vec1) == -14
+
+def test_negative(vec0: Vector):
+    assert -vec0 == Vector([-1, -2, -3])
 
 
 if __name__ == "__main__":
     vec_0 = Vector([1, 2, 3])
     vec_1 = Vector([-1, -2 , -3])
+    vec_0[0] = 2
+    print(vec_0)
 
     print(1 + 3)
     print(vec_0 + vec_1)
