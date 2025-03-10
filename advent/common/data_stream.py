3785d59db13ab7ec98c51f14aefd4cb3dac9b798
Joseph D. Murphree <joseph.murphree@protonmail.com>
2025-03-10 10:08:04 -0500

    created CharPosition class and added it to data_stream.py

--- advent/common/data_stream.py
+++ advent/common/data_stream.py
@@ -1,12 +1,17 @@
-# Copied from Matt
+# Modified from Matt
 
 from pathlib import Path
-from typing import Generator, TypeVar, Callable, Iterator, TypeAlias
+from typing import Generator, TypeVar, Callable, Iterator, NamedTuple
+
+from advent.common.vectors import Vector
 
 T = TypeVar("T")
 U = TypeVar("U")
 
-CharData: TypeAlias = tuple[int, int, str]
+
+class CharPosition(NamedTuple):
+    position: Vector
+    char: str
 
 
 def stream_lines_from_file(f_path: Path) -> Generator[str, None, None]:
@@ -14,20 +19,24 @@ def stream_lines_from_file(f_path: Path) -> Generator[str, None, None]:
         while line := f.readline():
             yield line.rstrip()
 
+
 def parse_line_to_ints(line: str) -> Generator[int, None, None]:
     for i in line.split():
         yield int(i)
 
+
 def pipe(
-        in_stream: Generator[T, None, None],
-        out_stream_gen: Callable[[T], Generator[U, None, None]]
+    in_stream: Generator[T, None, None],
+    out_stream_gen: Callable[[T], Generator[U, None, None]],
 ) -> Generator[U, None, None]:
     for data in in_stream:
-        yield from out_stream_gen(data)        
+        yield from out_stream_gen(data)
+
 
 def stream_position_and_char(
-    file_data: Iterator[str],
-) -> Iterator[CharData]:  # TODO: move this to common
+    f_path: Path,
+) -> Iterator[CharPosition]:
+    file_data = stream_lines_from_file(f_path)
     for line_num, line in enumerate(file_data):
         for char_num, char in enumerate(line):
-            yield (line_num, char_num, char)
+            yield CharPosition(Vector([line_num, char_num]), char)
