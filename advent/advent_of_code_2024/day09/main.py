b045bbb233ba3506572e63be770a0d72818cb0c6
Joseph D. Murphree <joseph.murphree@protonmail.com>
2025-03-11 18:20:09 -0500

    started building helper functions for expand_diskmap

--- advent/advent_of_code_2024/day09/main.py
+++ advent/advent_of_code_2024/day09/main.py
@@ -1,8 +1,26 @@
-def expand_diskmap():
+def create_file_block(block_size: int, block_id: int) -> str:
+    return block_size * str(block_id)
+
+
+def create_free_block(block_size: int) -> str:
+    return block_size * "."
+
+
+def parse_file_free_pair(
+    file_block_size: int, free_block_size: int, block_id: int
+) -> str:
+    file_block = create_file_block(file_block_size, block_id)
+    free_block = create_free_block(free_block_size)
+    return file_block + free_block
+
+
+def expand_diskmap(diskmap: str):
     pass
+
 
 def find_checksum():
     pass
+
 
 def exercise_one():
     pass
