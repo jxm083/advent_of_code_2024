d06168393e0f7c85281bc2a38b3aa2984bced337
Joseph D. Murphree <joseph.murphree@protonmail.com>
2025-03-21 15:58:29 -0500

    fixed type issues in vectors with __future__ annotations

--- advent/common/vectors.py
+++ advent/common/vectors.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from collections.abc import Sequence
 from typing import TypeVar, Union, SupportsIndex
 
@@ -5,30 +7,30 @@ T = TypeVar("T")
 
 
 class Vector(tuple[int, ...]):
-    def __add__(self, other: tuple[int, ...]) -> "Vector":
+    def __add__(self, other: tuple[int, ...]) -> Vector:
         return Vector([x + y for x, y in zip(self, other)])
 
-    def __sub__(self, other: "Vector") -> "Vector":
+    def __sub__(self, other: Vector) -> Vector:
         return Vector([x - y for x, y in zip(self, other)])
 
-    def __mul__(self, other: Union[int, tuple[int, ...], SupportsIndex]) -> "Vector":
+    def __mul__(self, other: Union[int, tuple[int, ...], SupportsIndex]) -> Vector:
         if isinstance(other, int):
             return Vector([other * x for x in self])
-        elif isinstance(other, "Vector"):
+        elif isinstance(other, Vector):
             return Vector([x * y for x, y in zip(self, other)])
         else:
             raise ValueError("Not a valid comparison")
 
-    def __rmul__(self, other: Union["Vector", int, SupportsIndex]) -> "Vector":
+    def __rmul__(self, other: Union[Vector, int, SupportsIndex]) -> Vector:
         if isinstance(other, int):
             return Vector([other * x for x in self])
-        elif isinstance(other, "Vector"):
+        elif isinstance(other, Vector):
             return Vector([x * y for x, y in zip(self, other)])
         else:
             raise ValueError("Not a valid comparison")
 
-    def __eq__(self, other: Union["Vector", object]) -> bool:
-        if isinstance(other, "Vector"):
+    def __eq__(self, other: Union[Vector, object]) -> bool:
+        if isinstance(other, Vector):
             equal = True
             for x, y in zip(self, other):
                 if x != y:
